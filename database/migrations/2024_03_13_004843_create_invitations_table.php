4a97c97e92e6755637dfe2e1797708960d351c3f
HenLeiko <jioiiika1337@gmail.com>
2024-03-15 02:11:10 +0300

    add migration for invitations

---
+++ database/migrations/2024_03_13_004843_create_invitations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('telegram_users');
+            $table->string('title')->nullable();
+            $table->string('code')->nullable();
+            $table->string('club')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
