3785fe7d0aba80c2445cc1ce6f310ad26724ef3a
HenLeiko <jioiiika1337@gmail.com>
2024-03-17 13:14:00 +0300

    add handle message

--- app/Telegram/Command/CreateCertCommand.php
+++ app/Telegram/Command/CreateCertCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Telegram\Command;
 
+use App\Models\TelegramUser;
 use Telegram\Bot\Commands\Command;
 
 class CreateCertCommand extends Command
@@ -10,6 +11,11 @@ class CreateCertCommand extends Command
     protected string $description = 'Создать сертификат на определённую сумму';
     public function handle()
     {
+        $this->replyWithMessage([
+            'text' => 'Укажите номинал сертификата, пример: "2000"'
+        ]);
+        TelegramUser::where('user_id', '=', $this->getUpdate()->message->from->id)->update(['status' => 'add_cert_value']);
+
         // TODO: Implement handle() method.
     }
 }
