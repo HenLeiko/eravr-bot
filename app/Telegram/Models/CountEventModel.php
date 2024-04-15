0afb58332508784930a98d840bbd8c0ebc252a13
HenLeiko <jioiiika1337@gmail.com>
2024-04-15 16:44:13 +0300

    refactoring EventModel

--- app/Telegram/Models/CountEventModel.php
+++ app/Telegram/Models/CountEventModel.php
@@ -7,13 +7,15 @@ use Carbon\Carbon;
 use Spatie\GoogleCalendar\Event;
 use Telegram\Bot\BotsManager;
 use Telegram\Bot\Laravel\Facades\Telegram;
-use Telegram\Bot\Objects\Message;
+use function Pest\Laravel\get;
 
 class CountEventModel
 {
     protected string $date;
     protected BotsManager $botsManager;
-
+    protected array $countEvents;
+    protected array $calendars = ['ou86tdr91vgt3orsdtsnbu31r8@group.calendar.google.com', '56vb14ndiolefkv6b88h4urrfc@group.calendar.google.com', 'eravrr@gmail.com'];
+    protected array $events;
 
     public function eventCounter(): void
     {
@@ -21,48 +23,37 @@ class CountEventModel
         $eventsMolodega = Event::get(Carbon::parse($this->date), Carbon::today(), [], 'ou86tdr91vgt3orsdtsnbu31r8@group.calendar.google.com');
         $eventsSelega = Event::get(Carbon::parse($this->date), Carbon::today(), [], '56vb14ndiolefkv6b88h4urrfc@group.calendar.google.com');
         $eventsBelyaevo = Event::get(Carbon::parse($this->date), Carbon::today(), [], 'eravrr@gmail.com');
-        $countEvents = array();
-        foreach ($eventsSelega as $event) {
-            if (property_exists($event->googleEvent, 'creator')) {
-                if (!isset($countEvents[$event->creator->email])) {
-                    $countEvents[$event->creator->email] = 0;
-                }
-                $countEvents[$event->creator->email] += 1;
-            }
-        }
-        foreach ($eventsBelyaevo as $event) {
-            if (property_exists($event->googleEvent, 'creator')) {
-                if (!isset($countEvents[$event->creator->email])) {
-                    $countEvents[$event->creator->email] = 0;
-                }
-                $countEvents[$event->creator->email] += 1;
-            }
-        }
-        foreach ($eventsMolodega as $event) {
-            if (property_exists($event->googleEvent, 'creator')) {
-                if (!isset($countEvents[$event->creator->email])) {
-                    $countEvents[$event->creator->email] = 0;
-                }
-                $countEvents[$event->creator->email] += 1;
-            }
-        }
-        $this->sendResponse($countEvents);
+        $this->iterationCount($eventsMolodega);
+        $this->iterationCount($eventsSelega);
+        $this->iterationCount($eventsBelyaevo);
+        $this->sendResponse();
         TelegramUser::where('user_id', '=', Telegram::getWebhookUpdate()->message->chat->id)->update(['status' => 'none']);
         // TODO: добавить выгрузку в таблицу
     }
 
-    private function sendResponse(array $countEvents): Message
+    private function sendResponse()
     {
         Telegram::sendChatAction([
             'chat_id' => Telegram::getWebhookUpdate()->message->chat->id,
             'action' => 'typing'
         ]);
-        foreach ($countEvents as $key => $event) {
+        foreach ($this->countEvents as $key => $event) {
             $response = Telegram::sendMessage([
                 'chat_id' => Telegram::getWebhookUpdate()->message->chat->id,
                 'text' => $key . ' создал ' . $event . ' записей.'
             ]);
         }
-        return $response;
+    }
+
+    private function iterationCount($events): void
+    {
+        foreach ($events as $event) {
+            if (property_exists($event->googleEvent, 'creator')) {
+                if (!isset($this->countEvents[$event->creator->email])) {
+                    $this->countEvents[$event->creator->email] = 0;
+                }
+                $this->countEvents[$event->creator->email] += 1;
+            }
+        }
     }
 }
