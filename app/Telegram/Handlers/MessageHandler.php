1e69fa15b39f85440f601ddacc89b6077de8f08a
henleiko <jioiiika1337@gmail.com>
2024-10-09 13:51:18 +0300

    add layout for message handler

---
+++ app/Telegram/Handlers/MessageHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Telegram\Handlers;
+
+use App\Telegram\Interfaces\HandlerInterface;
+
+class MessageHandler implements HandlerInterface
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+
+    public function getException()
+    {
+        // TODO: Implement getException() method.
+    }
+}
