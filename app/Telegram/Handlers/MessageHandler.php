2a7e244ba5570c42b9758d0d7e7859ade3eb88f4
henleiko <jioiiika1337@gmail.com>
2024-11-26 21:41:22 +0300

    add function for get command from mapping

--- app/Telegram/Handlers/MessageHandler.php
+++ app/Telegram/Handlers/MessageHandler.php
@@ -3,26 +3,48 @@
 namespace App\Telegram\Handlers;
 
 use App\Models\TelegramUser;
-use App\Services\CertService;
+use App\Services\CommandFactoryService;
 use App\Telegram\Interfaces\HandlerInterface;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class MessageHandler implements HandlerInterface
 {
-    protected CertService $certService;
-    protected $user;
+    protected CommandFactoryService $commandFactory;
+
     public function __construct()
     {
-        $user = TelegramUser::where('user_id', '=', Telegram::getWebhookUpdate()->message->from->id)->first();
-        if ($user || !isset(Telegram::getWebhookUpdate()->message->message_thread_id)) {
-            $this->user = $user;
-        }
-        $this->certService = new CertService();
+        $this->commandFactory = new CommandFactoryService();
     }
 
     public function handle(): void
     {
-        // TODO: Implement handle() method.
+        $message = Telegram::getWebhookUpdate()->message;
+        $command = $this->getCommandFromMapping($message);
+
+        if($command) {
+            $commandService = $this->commandFactory->getServiceFromCommand($command);
+
+            if($commandService) {
+                $commandService->start();
+            } else {
+                print_r('Command not found');
+            }
+        }
+
+    }
+
+    protected function getCommandFromMapping($message)
+    {
+        $commands = [
+            'создать приглашение' => 'create_invite',
+            'создать сертификат' => 'create_cert'
+        ];
+
+        foreach ($commands as $key => $command) {
+            if (mb_stripos($message->text, $key) !== false) {
+                return $command;
+            }
+        }
     }
 
     public function getException()
