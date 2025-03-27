30408b372fa8a23b66931ab3ad8219f7ec11d973
henleiko <jioiiika1337@gmail.com>
2025-03-27 18:43:55 +0300

    add hardcode redirect for counter

--- app/Services/CertService.php
+++ app/Services/CertService.php
@@ -2,17 +2,95 @@
 
 namespace App\Services;
 
-use App\Models\TelegramUser;
+
+use App\Models\Certificate;
+use App\Models\UserState;
+use DantSu\PHPImageEditor\Image;
+use Google\Service\Transcoder\Input;
+use phpseclib3\Math\BigInteger;
+use Telegram\Bot\FileUpload\InputFile;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class CertService
 {
-    protected $update;
 
-    public function setValue()
+    public function start($userId, $chatId, $message): void
     {
-        $certValue = [
-            'user_id' => $this->user->id,
-        ];
+        Telegram::sendMessage([
+            'chat_id' => $chatId,
+            'text' => 'Для начала создания сертификата укажите имя!',
+        ]);
+        $userState = UserState::updateOrCreate([
+            'user_id' => $userId,
+        ],
+            [
+                'user_id' => $userId,
+                'command' => 'create_cert',
+                'state' => 'set_value',
+                'data' => [
+                    'value' => '',
+                    'code' => '',
+                ],
+            ]
+        );
+        print_r('сервис создания сертификата');
+    }
+
+    public function handle($userId, $chatId, $message): void
+    {
+        print_r('Параметр прошёл');
+        $userState = UserState::where('user_id', $userId)->first();
+        $state = $userState->state;
+
+        if (!$userState) {
+            return;
+        }
+
+        switch ($state) {
+            case 'set_value':
+                $userState->state = 'set_code';
+                $userState->data = array_merge($userState->data ?? [], ['value' => $message->text]);
+                $userState->save();
+                $this->getResponse($chatId, 'Укажите код сертификата формата: АНДРЕЙ241224-1');
+                break;
+            case 'set_code':
+                $userState->data = array_merge($userState->data ?? [], ['code' => $message->text]);
+                $userState->state = 'done';
+                $userState->save();
+                $result = $this->createCertPicture($userState, $chatId);
+                $result ? $this->getResponse($chatId, 'Сертификат успешно создан! :)') : $this->getResponse($chatId, 'Произошла ошибка во время отправки сертификата :(');
+                break;
+            default: echo 'qwe';
+        }
+    }
+
+    private function getResponse(int $chatId, String $text): void
+    {
+        $result = Telegram::sendMessage([
+            'chat_id' => $chatId,
+            'text' => $text
+        ]);
+    }
+
+    private function createCertPicture($userState, $chatId)
+    {
+        $imageName = uniqid();
+        $value = $userState->data['value'];
+        $code = $userState->data['code'];
+        $code = mb_strtoupper($code);
+        $path = storage_path('app/telegram/Сертификат.png');
+        $font = storage_path('app/telegram/Montserrat-Regular.ttf');
+        $savePath = storage_path('app/telegram-temps');
+
+        print_r( $path);
+        Image::fromPath($path)
+            ->writeText($value . ' ₽',  $font, 70, '#FFFFFF', '745', '238')
+            ->writeText($code, $font, 18, '000000', '870', '960', Image::ALIGN_CENTER, Image::ALIGN_MIDDLE, 0)
+            ->savePNG($savePath . $imageName . '.png');
+        return Telegram::sendDocument([
+            'chat_id' => $chatId,
+            'document' => InputFile::create($savePath . $imageName . '.png')
+        ]);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\WebhookController;
+use DigitalStars\Sheets\DSheets;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +21,17 @@ Route::get('/', function () {
 });
 
 Route::post('/webhook', WebhookController::class);
+
+
+///
+Route::get('/eravr', function (Request $request) {
+    $club = $request->query('club');
+    $config = Storage::path('google-calendar/calendar-419415-3bb51b0d7788.json');
+    print_r($config);
+    $spreadsheet_id = '10EyxVi9MHMwTpQS6oW21D66FEJcnqE6tsaAAqaz0WYk';
+    $sheet = DSheets::create($spreadsheet_id, '../storage/app/google-calendar/calendar-419415-3bb51b0d7788.json')->setSheet('Лист1');
+    $sheet->append([[$club, date("d.m.Y H:i")]]);
+    
+});
 
 //require __DIR__.'/auth.php';
