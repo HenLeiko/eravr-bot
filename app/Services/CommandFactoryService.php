a4494b4009a68a17d1fa87bb7687cf5efff8839f
henleiko <jioiiika1337@gmail.com>
2025-03-29 01:31:56 +0300

    add inviteService container into mapping

--- app/Services/CommandFactoryService.php
+++ app/Services/CommandFactoryService.php
@@ -7,14 +7,16 @@ use Illuminate\Contracts\Foundation\Application;
 class CommandFactoryService
 {
     /**
+     * Простая фабрика для создания экземпляра сервиса команды
+     *
      * @param string $command
-     * @return string|Application|null
+     * @return string|Application|CertService|null
      */
-    public function getServiceFromCommand(string $command): string|Application|null|CertService
+    public function getServiceFromCommand(string $command): string|Application|null|CertService|InviteService
     {
         return match ($command) {
             'create_cert' => app(CertService::class),
-            'create_invite' => 'lorem',
+            'create_invite' => app(InviteService::class),
             default => null,
         };
     }
