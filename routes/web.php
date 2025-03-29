507e723abdef511f12f0433c6d832683730cf681
henleiko <jioiiika1337@gmail.com>
2025-03-29 13:56:43 +0300

    add route param for few bots

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/webhook', WebhookController::class);
+Route::post('/webhook/{botName}', WebhookController::class);
 
 
 ///
@@ -31,7 +31,7 @@ Route::get('/eravr', function (Request $request) {
     $spreadsheet_id = '10EyxVi9MHMwTpQS6oW21D66FEJcnqE6tsaAAqaz0WYk';
     $sheet = DSheets::create($spreadsheet_id, '../storage/app/google-calendar/calendar-419415-3bb51b0d7788.json')->setSheet('Лист1');
     $sheet->append([[$club, date("d.m.Y H:i")]]);
-    
+
 });
 
 //require __DIR__.'/auth.php';
