2ac745eabc38e14fe8f1cba1149b926d92bb3358
henleiko <jioiiika1337@gmail.com>
2025-06-27 15:59:24 +0300

    refactoring migrations

--- database/migrations/2025_03_30_200634_create_telegram_channel_members_table.php
+++ database/migrations/2025_03_30_200634_create_telegram_channel_members_table.php
@@ -13,16 +13,28 @@ return new class extends Migration
     {
         Schema::create('telegram_channel_members', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('chat_id')->nullable()->comment('ID чата или канала');
-            $table->unsignedBigInteger('user_id')->comment('ID пользователя');
-            $table->string('status', 20)->comment('Статус пользователя: member|administrator|restricted|left|kicked');
-            $table->string('ref_code', 8)->comment('Реферальный код');
+            $table->unsignedBigInteger('chat_id')
+                ->nullable()
+                ->comment('ID чата или канала');
+            $table->unsignedBigInteger('user_id')
+                ->comment('ID пользователя');
+            $table->string('status', 20)
+                ->comment('Статус пользователя: member|administrator|restricted|left|kicked');
+            $table->string('ref_code', 8)
+                ->nullable()
+                ->default(null)
+                ->comment('Реферальный код');
             $table->foreignId('ref_by')
                 ->nullable()
                 ->constrained('telegram_channel_members')
                 ->nullOnDelete();
-            $table->dateTime('timeout')->nullable()->comment('Ограничение на участие в конкурсах');
-            $table->boolean('is_participating')->default(false)->comment('Метка участия в конкурсе');
+            $table->dateTime('timeout')
+                ->nullable()
+                ->default(null)
+                ->comment('Ограничение на участие в конкурсах');
+            $table->boolean('is_participating')
+                ->default(false)
+                ->comment('Метка участия в конкурсе');
             $table->timestamps();
 
             $table->unique(['chat_id', 'user_id'], 'unique_telegram_channel_members');
